35c5a398b68eec3e2333c0c4e9ab49729140f767
danielsdeleo <dan@opscode.com>
2013-09-20 14:02:21 -0700

    Add tests for base config generator behavior

---
+++ spec/bootstrapper/config_generator_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'shared/config_generator'
+require 'bootstrapper/config_generator'
+
+describe Bootstrapper::ConfigGenerator do
+
+  include_examples "A Config Generator"
+
+end
---
+++ spec/shared/config_generator.rb
@@ -0,0 +1,33 @@
+shared_examples_for "A Config Generator" do
+  let(:config_generator) { described_class.new }
+
+  it "keeps a list of files to install" do
+    expect(config_generator.files_to_install).to be_empty
+  end
+
+  it "implements a prepare method" do
+    expect(config_generator).to respond_to(:prepare)
+  end
+
+  it "implements an install_config method" do
+    expect(config_generator).to respond_to(:install_config)
+  end
+
+  describe "when adding a file to the list of config files" do
+    before do
+      config_generator.install_file('data bag secret', "secret.pem") do |file|
+        file.content = "a bunch of gibberish"
+        file.mode = "0600"
+      end
+    end
+
+    it "describes the files content and permissions" do
+      expect(config_generator.files_to_install).to have(1).item
+      file = config_generator.files_to_install.first
+      expect(file.description).to eq("data bag secret")
+      expect(file.rel_path).to eq("secret.pem")
+      expect(file.content).to eq("a bunch of gibberish")
+      expect(file.mode).to eq("0600")
+    end
+  end
+end
