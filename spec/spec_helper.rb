992a8d3192c74cfef4a4cf1494b204960597b1cb
danielsdeleo <dan@opscode.com>
2013-09-24 14:40:38 -0700

    Add :focus filtering for rspec examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 $:.unshift File.expand_path("../lib", __FILE__)
 
 require 'bootstrapper'
+
+RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+end
